b39740cb175693d6c4cdb6b5be857a3413c3b099
Asa Huang <ahuang@unc.edu>
2021-08-31 18:34:22 -0400

    Progress on Exercise 1

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -4,7 +4,7 @@ __author__ = "730462854"
 
 num1: int = int(input("Left-hand side: "))
 num2: int = int(input("Right-hand side: "))
-print(str(num1) + " < " + str(num2) + " is False")
-print(str(num1) + " >= " + str(num2) + " is True")
-print(str(num1) + " == " + str(num2) + " is True")
-print(str(num1) + " != " + str(num2) + " is False")
+print(str(num1) + " < " + str(num2) + " is " + str(num1 < num2))
+print(str(num1) + " >= " + str(num2) + " is " + str(num1 >= num2))
+print(str(num1) + " == " + str(num2) + " is " + str(num1 == num2))
+print(str(num1) + " != " + str(num2) + " is " + str(num1 != num2))
