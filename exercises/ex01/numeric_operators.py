e6b4dc500a734ea7c0ec88cfd1af83b2591c950e
Asa Huang <ahuang@unc.edu>
2021-08-31 18:24:56 -0400

    Progress on Exercise 1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,8 +2,8 @@
 
 __author__ = "730462854"
 
-num1 = int(input("Left-hand side: "))
-num2 = int(input("Right-hand side: "))
+num1: int = int(input("Left-hand side: "))
+num2: int = int(input("Right-hand side: "))
 print(str(num1) + " ** " + str(num2) + " is " + str(num1 ** num2))
 print(str(num1) + " / " + str(num2) + " is " + str(num1 / num2))
 print(str(num1) + " // " + str(num2) + " is " + str(num1 // num2))
